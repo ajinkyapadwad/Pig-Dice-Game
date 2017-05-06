3f870eced5d1b1d4b8bc000fbdf3029302774058
Ajinkya Padwad <ajinkyapadwad@gmail.com>
2017-05-06 13:49:22 -0400

    new

--- app.js
+++ app.js
@@ -92,8 +92,8 @@ function init() {
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
-    document.getElementById('name-0').textContent = 'Player 1';
-    document.getElementById('name-1').textContent = 'Player 2';
+    document.getElementById('name-0').textContent = 'Player A';
+    document.getElementById('name-1').textContent = 'Player B';
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
     document.querySelector('.player-0-panel').classList.remove('active');
